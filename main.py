406ca42703f123b2497b49e0c46e9c59884455b0
Garbo <i@garbo.me>
2024-12-23 15:06:51 +0800

    chore:修改流程 可在config中写入城市名

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from scraper.job_scraper import JobScraper
 from utils.search_code import search_code
-from config.config import QUERY
+from config.config import QUERY,CITY_NAME
 if __name__ == "__main__":
-    search_name = input("请输入城市名称进行搜索: ").strip()
-    site_code = search_code('site.json', search_name)
+    if CITY_NAME == "":
+        search_name = input("请输入城市名称进行搜索: ").strip()
+        site_code = search_code('site.json', search_name)
+    else:
+        site_code = search_code('site.json', CITY_NAME)
 
     if site_code == 0:
         site_code = 101280600  # 默认城市代码
